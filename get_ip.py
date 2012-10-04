a8b03ea55903e145b9f0e3f9f1d3172807030a19
hansoncao@hotmail.com <g>
2012-10-04 14:40:27 +0930

    记录IP  txt
    记录IP ..每次写入一行…   \n

--- get_ip.py
+++ get_ip.py
@@ -1,29 +1,53 @@
 #  create to get Dynamic IP
 import socket
-
+import os
 class GetIp(object):
 
     
-    store_path = ''
+    global path
+    path = '/Users/ahaic/Desktop/ip_log.txt'
 
     def __init__(self,url):
 
         self.url=url
         self.ip=''
+        self.get_ip()
+        self.record_ip()
 
     def get_ip(self):
 
         self.ip=socket.gethostbyname_ex(self.url)
 
     def record_ip(self):
+        
+        
+            
+        if os.path.isfile(path) !=True:
 
-        pass
+            print('file does not exists ')
+            try:
+                ip_log = open(path,'w')
 
+            except IOError:
+                print('fucked')
+               
+        else:
+            try:
+                
+                ip_log=open(path,'a')
+            except:
+                print(' open file failed in a mode')
+            else:
+                print('file open successfully')
 
+        ip_log.writelines(self.ip[2])
+        ip_log.write('\n')
+        ip_log.close()
+
+        print('written')
+
+        
 obj = GetIp('paksila.xicp.net')
-
-output_ip = obj.get_ip()
-
 
 
 print('ip address: ', obj.ip[2])
