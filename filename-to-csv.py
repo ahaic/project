16091af24a135333c475cc10ef30bc588b365bf5
ahaic <ahaic@users.noreply.github.com>
2019-06-29 23:48:00 +0800

    Update filename-to-csv.py
    now it list all filenames and video duration into csv

--- filename-to-csv.py
+++ filename-to-csv.py
@@ -1,34 +1,40 @@
 #coding:utf8
 #29 June 2019
 # for the purpsoe of scanning all video names and making schedule
+# list filenames and video durations 
 
 import os,csv
-import sys
+import sys,cv2
+import subprocess
 
+
+def get_video_duration(filename):
+    try:
+      cap = cv2.VideoCapture(filename)
+      if cap.isOpened():
+        rate = cap.get(5)
+        frame_num =cap.get(7)
+        #print(frame_num,rate)
+        duration = frame_num/rate/60
+        return duration
+    except cv2.error as e:
+        print('not video file,ingore',e)
 
 
 print('current dir:', os.getcwd())
-
 path=os.getcwd()
-f = []
+
+f = dict()
 for (dirpath, dirnames, filenames) in os.walk(path):
-    f.extend(filenames)
+#    f.extend(filenames)
+    for i in filenames:
+        f[i]=get_video_duration(os.path.join(dirpath,i))
+#    print(f)
     break
-
-print(type(f))
-
 
 
 with open('output.csv', 'w',newline='', encoding='utf-8-sig') as file:
     writer = csv.writer(file)
-
-#    writer.writerows([f])
-
-    for row in f:
-    #    print(row)
-    #    print(type(row))
-
-
-        writer.writerow([row])
-
+    for (key,value) in f.items():
+        writer.writerow([key,value])
     print('finish')
