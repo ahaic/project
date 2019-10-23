32789b610a360417b09350a07cfbef923520ea90
ahaic <ahaic@users.noreply.github.com>
2019-10-23 23:50:14 +0800

    Update opencv-python.sh

--- opencv-python.sh
+++ opencv-python.sh
@@ -20,12 +20,12 @@ echo ""
 
 echo "installing python-opencv for python3"
 
-sudo apt-get install libatlas-base-dev -y 1>/dev/null 2>&1
-sudo apt-get install libjasper-dev -y 1>/dev/null 2>&1
-sudo apt-get install libilmbase-dev libopenexr-dev libgstreamer1.0-dev -y  1>/dev/null 2>&1
-apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev -y  1>/dev/null 2>&1
-sudo apt-get install libqt4-test -y 1>/dev/null 2>&1
-pip3 install opencv-python -y 1>/dev/null 2>&1
+sudo apt-get install libatlas-base-dev -y 
+sudo apt-get install libjasper-dev -y
+sudo apt-get install libilmbase-dev libopenexr-dev libgstreamer1.0-dev -y  
+apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev -y  
+sudo apt-get install libqt4-test -y
+pip3 install opencv-python -y 
 
 
 
